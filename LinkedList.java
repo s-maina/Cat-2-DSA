a4d8ef12000f413fd6b34269b8c8a1e6b33cb19f
s-maina <shatra.maina@strathmore.edu>
2025-03-22 23:42:11 +0300

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,62 @@
+public class LinkedList {
+    private Node head;
+
+    private static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Insert a node at the beginning
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Insert a node at the end
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    // Delete a node from the beginning
+    public void deleteFromBeginning() {
+        if (head != null) {
+            head = head.next;
+        }
+    }
+
+    // Displaying the list
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertAtBeginning(10);
+        list.insertAtBeginning(20);
+        list.insertAtEnd(30);
+        list.display(); 
+
+        list.deleteFromBeginning();
+        list.display(); 
+    }
+}
